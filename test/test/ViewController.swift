13a57360e290f9de7bed345c8fcf23eca7db9744
徐兆陽 <wall1350@gmail.com>
2020-07-16 18:18:49 +0800

    add

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("lily")
         print("uffi")
-        print("daniel")
+        
     }
 
 
