7283c337f768a27b36fa08205382077a254cb4d4
徐兆陽 <wall1350@gmail.com>
2020-07-16 18:06:39 +0800

    init
    這是三位第一次用git

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 徐兆陽 on 2020/7/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
